a86d13feb7a47ad11850a5fc0d0d02bb83d0f440
Yuno <138690958+oyunodev@users.noreply.github.com>
2024-01-11 20:31:11 -0300

    Add files via upload

---
+++ blocosmarioex2.c
@@ -0,0 +1,34 @@
+int main(void)
+{
+    int innumero;
+    
+    do
+    {
+        printf("Quantidade de linhas: ");
+        scanf("%i", &innumero);
+    } 
+    while (innumero < 1 || innumero > 8);
+    for (int i = 0; i < innumero; i++)
+    {
+        for (int j = innumero - 1; j > i; j--)
+        {
+            printf(" ");
+        }
+        
+        for (int k = -1; k < i; k++)
+        { 
+            printf("#");
+        }
+        
+        printf(" ");
+
+        for (int l = -1; l < i; l++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+    
+    
+}
